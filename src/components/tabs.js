e0f5e403ba284246a0925155a3cb7d0c4e0ea9c1
Brian Nguyen <brian.edison.nguyen@gmail.com>
2022-11-09 19:58:29 -0800

    feat: tabs

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 const Tabs = (topics) => {
   // TASK 3
   // ---------------------
@@ -13,6 +14,17 @@ const Tabs = (topics) => {
   //   <div class="tab">technology</div>
   // </div>
   //
+  const topicsEl = document.createElement('div');
+  topicsEl.classList.add('topics');
+  console.log(topics)
+  topics.forEach(topic => {
+    const tabEl = document.createElement('div');
+    tabEl.classList.add('tab');
+    tabEl.textContent = topic;
+    topicsEl.append(tabEl);
+  })
+
+  return topicsEl;
 }
 
 const tabsAppender = (selector) => {
@@ -23,6 +35,13 @@ const tabsAppender = (selector) => {
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
   //
+  const URL = 'http://localhost:5001/api/topics'
+  axios.get(URL)
+       .then(res => {
+         const tabsContainer = document.querySelector(selector);
+         const topics = Tabs(res.data.topics);
+         tabsContainer.appendChild(topics);
+       })
 }
 
 export { Tabs, tabsAppender }
